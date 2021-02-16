65d63438341ab23390f72111cc1a3abb244c6ef3
Morteza Nedaei <nedaei1375@gmail.com>
2021-02-16 21:23:25 +0330

    add new constants

---
+++ app/src/main/java/com/mone/countup_timer_service/utils/Constants.kt
@@ -0,0 +1,16 @@
+package com.mone.countup_timer_service.utils
+
+object Constants {
+
+    const val CHANNEL_NAME = "CountUpTimerService"
+    const val CHANNEL_ID = "0"
+    const val NOTIFICATION_ID = 1
+
+    const val ACTION_NOTIFICATION_KEY = "ACTION_NOTIFICATION_KEY"
+    const val ACTION_NOTIFICATION_NAME = "ACTION_NOTIFICATION_NAME"
+    const val ACTION_START = "START"
+    const val ACTION_STOP = "STOP"
+    const val ACTION_TIME_KEY = "TIME_KEY"
+    const val ACTION_TIME_VALUE = "00:00:00"
+    const val ACTION_TIMER_STOP = "TIMER_STOP"
+}
